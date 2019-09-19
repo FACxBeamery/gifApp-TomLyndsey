452447bd33f37c434a5ebc3c1a54d0664170813a
Tom Galligan <tomgalligan@Toms-MacBook-Pro.local>
2019-09-19 16:59:27 +0100

    display multiple gifs

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
 
 // import apiKey from "config.js";
-
-
-function sendRequest() {
+// console.log(document.getElementById("submitrequest"))
+document.getElementById("submitrequest").addEventListener("click",(event => {
+  // get the input from that element
+  const query = document.getElementById("searchQuery").value;
+  sendRequest(query); // pass as param here
+}));
+ 
+function sendRequest(query) {
+  
     var xhr = new XMLHttpRequest();
 
     xhr.addEventListener("readystatechange", function () {
       if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log(xhr.responseText);
+        //console.log(xhr.responseText);
         response = JSON.parse(xhr.responseText);
         console.log(response);
         for (i=0;i<response.data.length;i++){
             url = response.data[i].images.original.url;
-            imgTemp = document.querySelector("#gifImage");
-            imgTemp.src = url;
+            // console.log(url)
+            // imgTemp = document.querySelector("#gifImage");
+            // imgTemp.src = url;
+
+            img = document.createElement("img");
+            img.src = url;
+            gif = document.getElementById("gifs")
+            gif.appendChild(img)
         }
         response.data
       }
     });
     
-    const query = document.getElementById("searchQuery").value;
+    
     console.log(query);
-    xhr.open("GET", `https://cors-anywhere.herokuapp.com/http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=1&rating=g`, true);
+    xhr.open("GET", `https://cors-anywhere.herokuapp.com/http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=10&rating=g`, true);
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.send();
 
