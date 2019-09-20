9d03b46c4914a33c87b9c01306e5e8e98acd308e
Tom Galligan <tomgalligan@Toms-MacBook-Pro.local>
2019-09-20 10:18:55 +0100

    more error handling

--- main.js
+++ main.js
@@ -1,57 +1,60 @@
-document.getElementById("submitrequest").addEventListener("click",(event => {
-  const query = document.getElementById("searchQuery").value;
-  sendRequest(query);
-}));
+document.getElementById ('submitrequest').addEventListener ('click', event => {
+  const query = document.getElementById ('searchQuery').value;
+  sendRequest (query);
+});
 
-function sendRequest(query) {
+function sendRequest (query) {
+  var xhr = new XMLHttpRequest ();
 
-    var xhr = new XMLHttpRequest();
-
-    xhr.addEventListener("readystatechange", function () {
-      removeChildNodes('gifs');
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        response = JSON.parse(xhr.responseText);
-        if (response.data.length === 0) {
-          errorMessage();
-          return
-        }
-        for (i=0;i<response.data.length;i++){
-            gifURL = response.data[i].images.original.url;
-            addGIF(gifURL);
+  xhr.addEventListener ('readystatechange', function () {
+    removeChildNodes ('gifs');
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      response = JSON.parse (xhr.responseText);
+      if (response.data.length === 0) {
+        errorMessage (
+          "Uh oh! We couldn't find any matches for that search. Please try again!"
+        );
+        return;
+      } else {
+        for (i = 0; i < response.data.length; i++) {
+          gifURL = response.data[i].images.original.url;
+          addGIF (gifURL);
         }
       }
-    });
+    } else if (xhr.readyState === 4 && xhr.status !== 200) {
+      errorMessage (
+        `Oh no! There\'s a server error. The error code is ${xhr.status}.`
+      );
+      return;
+    }
+  });
 
-    
-url = `https://cors-anywhere.herokuapp.com/http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=10&rating=g`;
+  let url = `https://cors-anywhere.herokuapp.com/http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=10&rating=g`;
 
-
-xhr.open("GET", url, true);
-xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest'); // avoids CORS error
-xhr.send();
-
+  xhr.open ('GET', url, true);
+  xhr.setRequestHeader ('X-Requested-With', 'XMLHttpRequest'); // avoids CORS error
+  xhr.send ();
 }
 
-function addGIF(url) {
-  img = document.createElement("img");
+function addGIF (url) {
+  img = document.createElement ('img');
   img.src = url;
-  gif = document.getElementById("gifs");
-  gif.appendChild(img);
+  gif = document.getElementById ('gifs');
+  gif.appendChild (img);
 }
 
-function removeChildNodes(id) {
-  node = document.getElementById(id)
-  if (node != null) { // i.e. element exists
+function removeChildNodes (id) {
+  node = document.getElementById (id);
+  if (node != null) {
+    // i.e. element exists
     while (node.firstChild) {
-      node.removeChild(node.firstChild);
+      node.removeChild (node.firstChild);
     }
   }
 }
 
-function errorMessage() {
-  elem = document.createElement("p");
-  elem.className = "error";
-  textNode = document.createTextNode('Uh oh! We couldn\'t find any matches for that search. Please try again!');
-  elem.appendChild(textNode);
+function errorMessage (msg) {
+  elem = document.querySelector ('.error');
+  textNode = document.createTextNode (msg);
+  elem.appendChild (textNode);
 }
-
